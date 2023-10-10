64fbe4ecb687525fad0109b42c3f4d2fdebde884
artyom_sheshkovas <sheshkovas12@yandex.ru>
2023-10-10 13:31:43 +0300

    PY mod.3

--- funcs.py
+++ funcs.py
@@ -1,14 +1,14 @@
 a = int(2)
 b = int(5)
-def func():
+def func_1():
     count = a + b
     print(count)
 
-func()
+func_1()
 
 
-def func_2():
-    count = b - a
-    print(count)
+#def func_2():
+    #count = b - a
+    #print(count)
 
-func_2()
+#func_2()
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-from funcs import func
+from funcs import func_1
+
